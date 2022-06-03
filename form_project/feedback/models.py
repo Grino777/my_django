d3d0184bd9ec55016f56478d1dd27ce40cd4fde9
Grino777 <bars_B_P@mail.ru>
2022-06-03 15:08:56 +0300

    work

--- form_project/feedback/models.py
+++ form_project/feedback/models.py
@@ -4,6 +4,10 @@ from django.core.validators import MinLengthValidator, MaxValueValidator, MinVal
 # Create your models here.
 
 class Feedback(models.Model):
+    class Meta:
+        verbose_name = 'Отзывы'
+        verbose_name_plural = 'Отзывы'
+        ordering = ['name']
     name = models.CharField(max_length=10, validators=[MinLengthValidator(3)])
     surname = models.CharField(max_length=60, validators=[MinLengthValidator(3)])
     feedback = models.TextField()
